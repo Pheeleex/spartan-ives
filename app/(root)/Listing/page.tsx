e2263f20c9f588b06559d8d75316d7952ceb024c
Pheeleex <ohemufelix@gmail.com>
2025-01-30 22:12:26 +0100

    eslint

--- app/(root)/Listing/page.tsx
+++ app/(root)/Listing/page.tsx
@@ -3,48 +3,47 @@ import { fetchListings } from '@/utils/actions/file.actions'
 import Image from 'next/image'
 import React from 'react'
 
-const page = async() => {
+const Page = async () => {
     const listings = await fetchListings()
     console.log(listings)
-  return (
-    <div>
-        <div className='header w-full h-[calc(100vh-100px)] relative'>
-            <Image src='/assets/images/manor.jpg' alt='manor' className='w-full h-full' width={1000} height={1000} />
-            <div className='textbox absolute left-10 top-20 w-[60%] bg-white text-brand p-10'>
-                <h1 className='h1'>
-                    Our Current Listing
-                </h1>
-                <p>
-    Discover a diverse range of properties designed to match your unique lifestyle and needs. 
-    Whether you're searching for your dream home, a smart investment, or a cozy rental, we have something for everyone.
-  </p>
+  
+    return (
+        <div>
+            <div className='header w-full h-[calc(100vh-100px)] relative'>
+                <Image src='/assets/images/manor.jpg' alt='manor' className='w-full h-full' width={1000} height={1000} />
+                <div className='textbox absolute left-10 top-20 w-[60%] bg-white text-brand p-10'>
+                    <h1 className='h1'>Our Current Listing</h1>
+                    <p>
+                        Discover a diverse range of properties designed to match your unique lifestyle and needs. 
+                        Whether you&apos;re searching for your dream home, a smart investment, or a cozy rental, we have something for everyone.
+                    </p>
+                </div>
+            </div>
+            <div className='flex flex-wrap gap-8 justify-center items-center mt-20'>
+                {
+                    listings?.map((listing, index) => (
+                        <div key={index} className='flex flex-col w-[25rem] h-[25rem] rounded-lg border-[1px] border-red-200'>
+                            <CustomSlider 
+                                items={listing.images}
+                                width={400}
+                                height={400}
+                            />
+                            <div className='desc flex flex-col p-8'>
+                                <div className='flex'>
+                                    <h1>{listing.ProjectName}</h1>
+                                    <h2>{listing.ProjectAmount}</h2>
+                                </div>
+                                <div className='flex'>
+                                    <h1>{listing.Service}</h1>
+                                    <h2>{listing.ProjectType}</h2>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
-        <div className='flex flex-wrap gap-8 justify-center items-center mt-20'>
-        {
-            listings?.map((listing, index)=> (
-                <div key={index} className='flex flex-col w-[25rem] h-[25rem] rounded-lg border-[1px] border-red-200'>
-                    <CustomSlider 
-                        items={listing.images}
-                        width={400}
-                        height={400}
-                    />
-                    <div className='desc flex flex-col p-8'>
-                        <div className='flex'>
-                            <h1>{listing.ProjectName}</h1>
-                            <h2>{listing.ProjectAmount}</h2>
-                        </div>
-                        <div className='flex'>
-                            <h1>{listing.Service}</h1>
-                            <h2>{listing.ProjectType}</h2>
-                        </div>
-                    </div>
-                </div>
-            ))
-        }
-        </div>
-    </div>
-  )
+    )
 }
 
-export default page
+export default Page
--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -17,80 +17,81 @@ const Reviews = () => {
             src: `/assets/images/aslem.jpg`,
             name: 'Aslem Talim',
             service: 'Tenant'
-    },
-    {
-        ratings: 5,
-        review: `Spartan Ives provided invaluable insights and helped me achieve a fantastic ROI on my real estate investments.`,
-        src: `/assets/images/mindy.jpg`,
-        name: 'Maria Adeniyi',
-        service: 'Tenant'
-},
-{
-    ratings: 5,
-    review: `Spartan Ives delivered outstanding service when selling my property. 
-                 Their attention to detail and expert negotiation skills secured a record-breaking price.`,
-    src: `/assets/images/Maria.jpg`,
-    name: 'Mindy Rhodes',
-    service: 'Landlord'
-}
-]
-  return (
-    <section className='border-red-200 border-[1px] h-full lg:h-[16rem]'>
-        <div className="review-container flex flex-col lg:flex-row items-center h-full  ">
-            <div className="describe border-r-[1px] border-red-200  w-full lg:w-[50%] h-full">
-                <div className="contain flex flex-col justify-center items-center p-8 gap-4">
-                <h2 className='h2 self-start'>Don't just take our word for it</h2>
-                <p>Our clients investors are at the heart of what we do – and their feedback speaks 
-                    volumes about the success of our property investment mission. Beautiful homes. Sustainable growth.</p>
-                <button className=' self-start p-4 rounded-md border-brand text-brand border-[2px] 
-                hover:bg-[#880808] hover:text-white hover:transition-all hover:duration-1000'>Read all reviews</button>
-                </div>
-            </div>
-            <Swiper
-            spaceBetween={50}
-            slidesPerView={1}
-            modules={[Autoplay, Pagination]}
-            autoplay={{
-                delay: 3500,
-                disableOnInteraction: false,
-              }}
-              pagination={{
-                clickable: true,
-              }}
-              className=' w-full lg:w-[50%] h-full'
-          >
-              
-            {
-                    reviews.map((review, index) => (
-                        <SwiperSlide key={index} className='swipe flex flex-col justify-center items-centerw-[50%] p-8 
-                        h-full bg-red-100 gap-4'>
-                        <Stars 
-                            rating={review.ratings}
-                        />
-                        <p className='font-semibold text-brand text-[16px] w-full'>
-                            {review.review} </p>
-                        <div className="profile flex  gap-2">
-                        <div className='rounded-full overflow-hidden border-2 shadow-md w-16 h-16'>
-                            <Image 
-                                src={review.src}
-                                alt='investor'
-                                width={100}
-                                height={200}
-                                className='w-full h-full object-cover'
-                            />
-                        </div>
-                        <div className='flex flex-col'>
-                                <h3 className='h3'>{review.name}</h3>
-                                <p className=''>{review.service}</p>
-                           </div>
-                        </div>
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
-        </div>
-    </section>
-  )
-}
+        },
+        {
+            ratings: 5,
+            review: `Spartan Ives provided invaluable insights and helped me achieve a fantastic ROI on my real estate investments.`,
+            src: `/assets/images/mindy.jpg`,
+            name: 'Maria Adeniyi',
+            service: 'Tenant'
+        },
+        {
+            ratings: 5,
+            review: `Spartan Ives delivered outstanding service when selling my property. 
+                Their attention to detail and expert negotiation skills secured a record-breaking price.`,
+            src: `/assets/images/Maria.jpg`,
+            name: 'Mindy Rhodes',
+            service: 'Landlord'
+        }
+    ];
 
-export default Reviews
+    return (
+        <section className='border-red-200 border-[1px] h-full lg:h-[16rem]'>
+            <div className="review-container flex flex-col lg:flex-row items-center h-full">
+                <div className="describe border-r-[1px] border-red-200 w-full lg:w-[50%] h-full">
+                    <div className="contain flex flex-col justify-center items-center p-8 gap-4">
+                        <h2 className='h2 self-start'>Don&apos;t just take our word for it</h2>
+                        <p>
+                            Our clients and investors are at the heart of what we do – and their feedback speaks 
+                            volumes about the success of our property investment mission. Beautiful homes. Sustainable growth.
+                        </p>
+                        <button className='self-start p-4 rounded-md border-brand text-brand border-[2px] 
+                            hover:bg-[#880808] hover:text-white hover:transition-all hover:duration-1000'>
+                            Read all reviews
+                        </button>
+                    </div>
+                </div>
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    modules={[Autoplay, Pagination]}
+                    autoplay={{
+                        delay: 3500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    className='w-full lg:w-[50%] h-full'
+                >
+                    {reviews.map((review, index) => (
+                        <SwiperSlide key={index} className='swipe flex flex-col justify-center items-center w-[50%] p-8 
+                        h-full bg-red-100 gap-4'>
+                            <Stars rating={review.ratings} />
+                            <p className='font-semibold text-brand text-[16px] w-full'>
+                                {review.review}
+                            </p>
+                            <div className="profile flex gap-2">
+                                <div className='rounded-full overflow-hidden border-2 shadow-md w-16 h-16'>
+                                    <Image 
+                                        src={review.src}
+                                        alt='investor'
+                                        width={100}
+                                        height={200}
+                                        className='w-full h-full object-cover'
+                                    />
+                                </div>
+                                <div className='flex flex-col'>
+                                    <h3 className='h3'>{review.name}</h3>
+                                    <p>{review.service}</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </section>
+    );
+};
+
+export default Reviews;
